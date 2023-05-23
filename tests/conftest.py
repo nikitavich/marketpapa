c85de4120f143a3aa811f952de0be756e731a61f
marketpapa <n.sklyarenko@marketpapa.ru>
2023-05-23 23:08:49 +0600

    Обновление автотестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,6 +39,7 @@ def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("no-sandbox")
     options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
     options.add_argument("--window-size=800,600")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Remote(
