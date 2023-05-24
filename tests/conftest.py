28c745d2e45d835945b99bc66521dddca6a6f13f
marketpapa <n.sklyarenko@marketpapa.ru>
2023-05-24 17:23:41 +0600

    Обновление автотестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,13 +37,8 @@ def driver():
         }
     }
     options = webdriver.ChromeOptions()
-    options.add_argument("no-sandbox")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
-    options.add_argument("--window-size=800,600")
-    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Remote(
-        command_executor="http://172.17.0.3:4444/wd/hub",
+        command_executor="http://172.17.0.3:4444/",
         options=options)
 
     yield driver
