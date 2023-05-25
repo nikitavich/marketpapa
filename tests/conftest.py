f4790174865f32b3cca75dc65dd08268e1ea715c
marketpapa <n.sklyarenko@marketpapa.ru>
2023-05-25 15:54:03 +0600

    Обновление автотестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,7 +38,7 @@ def driver():
     }
     options = webdriver.ChromeOptions()
     driver = webdriver.Remote(
-        command_executor="http://localhost:4444/wd/hub",
+        command_executor="http://172.17.0.1:4444/wd/hub",
         options=options)
 
     yield driver
