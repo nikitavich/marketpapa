f48bd0f1bac3716d9431199bd57c3577e2503d44
marketpapa <n.sklyarenko@marketpapa.ru>
2023-05-25 16:14:32 +0600

    Обновление автотестов

--- tests/smoke/test_smoke.py
+++ tests/smoke/test_smoke.py
@@ -2,9 +2,32 @@ import time
 from lib.base_case import BaseCase
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+
 
 
 class TestSmoke:
+    def test_testing(self):
+        capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "113.0",
+            "selenoid:options": {
+                "enableVideo": False,
+                "timeouts": "180"
+            }
+        }
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Remote(
+            command_executor="http://172.17.0.1:4444/wd/hub",
+            options=options)
+        driver.get('https://marketpapa.ru/login')
+        driver.find_element(By.NAME, 'phone').send_keys('+79877120164')
+        driver.find_element(By.NAME, 'password').send_keys('q1w2e3r4t5y6')
+        driver.find_element(By.XPATH, "//span[@class='sc-iJnaPW izGUBw']").click()
+        time.sleep(3)
+        assert driver.current_url == "https://marketpapa.ru/find", "Не работает авторизация"
+
+
     def test_authorization(self, driver):
         driver.get('https://marketpapa.ru/login')
         driver.find_element(By.NAME, 'phone').send_keys('+79877120164')
