9ce925b9901950ee14d424e247fc04980d5f9824
marketpapa <n.sklyarenko@marketpapa.ru>
2023-06-19 16:14:37 +0600

    Обновление автотестов

--- tests/backend/test_deposit.py
+++
@@ -1,7 +0,0 @@
-from lib.adv_endpoints import AdvEndpoints
-
-
-class TestDeposit(AdvEndpoints):
-    def test_deposit(self):
-        response = self.advendpoints.deposit(company_id=3499821, deposit=100, balance_type=1)
-        assert response.status_code == 200, f"Wrong status code! Status code is {response.status_code}"
