055bdb5d5ca0674084b818825f1e3bd47431a31b
marketpapa <n.sklyarenko@marketpapa.ru>
2023-07-06 14:30:36 +0600

    Обновление автотестов

--- start_autotests.py
+++ start_autotests.py
@@ -72,4 +72,6 @@ def send_message_to_telegram(part_of_testing: str, message: str):
 
 
 if __name__ == '__main__':
+    start_advertising_autotests()
+    start_internal_analytics_autotests()
     start_ui_smoke_autotests()
