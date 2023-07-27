13fc5c50fa8fc01920ddb3b1f7c051250eabf50e
Sklyarenko Nikita <sklyarenkonikita97@mail.ru>
2023-07-27 15:35:10 +0600

    second commit

--- lib/proxies.py
+++
@@ -1,7 +0,0 @@
-PROXIES = [
-        "http://zazycg:7xey1N@213.166.72.243:9435",
-        "http://zazycg:7xey1N@91.188.241.166:9965",
-        "http://zazycg:7xey1N@91.188.243.4:9606",
-        "http://zazycg:7xey1N@91.188.242.191:9795",
-        "http://zazycg:7xey1N@91.188.241.70:9832",
-]
--- tests/regress/test_registration.py
+++ tests/regress/test_registration.py
@@ -21,9 +21,7 @@ class TestRegistration:
         driver.get("https://dev.marketpapa.ru/login")
         i = 0
         while i < 2:
-            driver.find_element(By.NAME, 'phone').send_keys('+79877120164')
-            driver.find_element(By.NAME, 'password').send_keys('q1w2e3r4t5y6')
-            driver.find_element(By.CSS_SELECTOR, ".sc-iJKOTD.ksUpGB").click()
+          driver.find_element(By.CSS_SELECTOR, ".sc-iJKOTD.ksUpGB").click()
             time.sleep(1)
             driver.refresh()
             driver.find_element(By.XPATH, "//*[text()='ДенисК']").click()
